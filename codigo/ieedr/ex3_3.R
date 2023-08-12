1b254f55ada5e19613f974726d7356b5849c66e6
cpmachado <cpmachado@protonmail.com>
2023-08-12 17:28:11 +0100

    Correcção no código do exercício 3.3

--- codigo/ieedr/ex3_3.R
+++ codigo/ieedr/ex3_3.R
@@ -1,6 +1,6 @@
 xi <- read.csv(file.path("dataset", "ieedr", "irmaos.csv"))$x
 df <- data.frame(media = mean(xi), desvio = sd(xi))
-df <- transform(xmin = ceiling(media - desvio), xmax = flooring(media + desvio))
+df <- transform(df, xmin = ceiling(media - desvio), xmax = floor(media + desvio))
 print(df)
 
 png(file.path("imagem", "ieedr", "ex3_3.png"))
@@ -14,4 +14,4 @@ barplot(
   xaxs = "i"
 )
 abline(NULL, v = df$xmin, col = "red")
-abline(NULL, v = df$xmax, col = "red")
+abline(NULL, v = df$xmax + 1, col = "red")
