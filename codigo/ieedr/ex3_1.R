2e2ea772581ac3535db465924477ada9f5524b20
cpmachado <cpmachado@protonmail.com>
2023-08-12 01:23:55 +0100

    Migração do exercício de 3.1 para uma nova estrutura

---
+++ codigo/ieedr/ex3_1.R
@@ -0,0 +1,8 @@
+xi <- read.csv(file.path("dataset", "ieedr", "irmaos.csv"))$x
+df <- as.data.frame(table(xi), responseName = "ni")
+df <- transform(df, fi = prop.table(df$ni))
+write.csv(
+    df,
+    file.path("tabela", "ieedr", "ex3_1.csv"),
+    quote = FALSE
+)
--- recursos/ex3_1.R
+++
@@ -1,3 +0,0 @@
-irmaos <- read.csv("./dataset/irmaos.csv")$xi
-n <- length(irmaos)
-print(transform(table(irmaos), f_i = Freq / n))
